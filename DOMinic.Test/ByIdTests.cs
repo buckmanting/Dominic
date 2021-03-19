4025e410850554865eae440b45e9085c131e3b0a
Aaron Buckley <aaron.buckley@springernature.com>
2021-03-19 13:58:41 +0000

    Revert "Delete DOMinic.Test directory"
    This reverts commit ee3ca6942bc6f53ea292149410458cd72a31ab81.

---
+++ DOMinic.Test/ByIdTests.cs
@@ -0,0 +1,75 @@
+//using System;
+//using System.IO;
+//using System.Threading.Tasks;
+//using Xunit;
+
+//namespace Dominic.Test
+//{
+//    public class ByIdTests
+//    {
+//        public ByIDTests()
+//        {
+//            var currentDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent;
+//            var viewPath = $"{currentDirectory}/TestTemplates";
+//            Template.SetViewLocation(viewPath);
+//        }
+
+//        [Fact]
+//        public async Task ItCanGetById()
+//        {
+//            var sut = await Template.Render("MultipleUniqueIds.cshtml", new {TestText = "Hello World"});
+//            Assert.Equal("My Div One, Hello World", sut.GetOnly.ById("div-1").text);
+//        }
+
+//        [Fact]
+//        public async Task ItCanDoGood()
+//        {
+//            var sut = await Template.Render("Form.cshtml", new
+//            {
+//                TestText = "my form title"
+//            });
+//            // can i get this to ignore empty attributes
+//            Assert.Equal(true, (bool)sut.GetOnlyById("my-button").Attribute("disabled"));
+//        }
+
+//        [Fact]
+//        public async Task ItCanDoAnotherGood()
+//        {
+//            var sut = await Template.Render("Article.cshtml", new
+//            {
+//                Title = "<div>title</div>",
+//                Author = "Aaron > Arron < Aaron"
+//            });
+//            // can i get this to ignore empty attributes
+//            Assert.Equal(true, (bool)sut.GetOnlyById("my-button").Attribute("disabled"));
+//        }
+
+//        // [Fact]
+//        // public async Task ItCanWithComplexClasses()
+//        // {
+//        //     var sut = await Template.Render("ComplexClasses.cshtml", new {TestText = "Hello World"});
+//        //     var expectedClasses = new[]
+//        //     {
+//        //         "cool-class-a",
+//        //         "another-class",
+//        //         "col-12"
+//        //     };
+//        //     Assert.Equal(expectedClasses, sut.GetOnlyById("a-lot-of-classes").Attribute("class"));
+//        // }
+
+//        [Fact]
+//        public async Task ItThrowsAnErrorWhenMoreThanOneElementFound()
+//        {
+//            var sut = await Template.Render("MultipleDuplicateIds.cshtml", new {TestText = "Hello World"});
+//            Assert.Throws<InvalidOperationException>(() =>
+//                sut.GetOnlyById("div-1")); // should this be a more explicit error?
+//        }
+
+//        [Fact]
+//        public async Task ItReturnsNullWhenNoElementFound()
+//        {
+//            var sut = await Template.Render("MultipleUniqueIds.cshtml", new {TestText = "Hello World"});
+//            Assert.Null(sut.GetOnlyById("not-a-real-id"));
+//        }
+//    }
+//}
---
+++ DOMinic.Test/TestModels/TestModel.cs
@@ -0,0 +1,7 @@
+namespace Dominic.Test.TestModels
+{
+    public class TestModel
+    {
+        public string TestText;
+    }
+}
